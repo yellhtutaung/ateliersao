dfe3aec9ca29c76513d5a9f98715accb07d686b9
Yell Htut <yellhtut4@gmail.com>
2023-11-16 14:03:20 +0630

    real time code

--- index.php
+++ index.php
@@ -1,14 +1,32 @@
 <?php include 'includes/head.php'?>
+<style>
+    .left-text-card
+    {
+        background: #F0F0EC;
+    }
+</style>
 <body class="body-bg">
-
     <?php include 'includes/header.php'?>
     <div class="theme-container">
-        <div class="img-container">
+        <div class="img-container mb-5 pb-5">
             <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/s1.jpg" alt="">
         </div>
-
         <p class="text-center themes-title-fonts my-5"><b>BRAND STORY</b></p>
         <p class="themes-para-fonts my-5">Atelier Sao Design Inc is a luxury interior architecture. Founded in 2016 as int design studio, the Yangon-based an interdisciplinary brand has evolved into an interior rchitecture firm that is practicing the contemporary deFounded in 2016 as an interior design studio, the Yangon-based an interdisciplinary. The studio presently consists of a team of creative individuals ho share design expertise and inspiration through a wide range of skills such as Interior Design, Interior Architecture, Product Design, Visual Merchandising, Interior Styling, Creative Interior Branding,</p>
+
+        <div class="row">
+            <div class="col-sm-12 col-md-3 mr-0">
+                <div class="left-text-card">
+                    <p class="themes-para-fonts my-5">Atelier Sao Design Inc is a luxury interior architecture. Founded in 2016 as int design studio, the Yangon-based an interdisciplinary brand has evolved into an interior rchitecture firm that is practicing the contemporary deFounded in 2016 as an interior design studio, the Yangon-based an interdisciplinary.</p>
+                </div>
+            </div>
+            <div class="col-sm-12 col-md-9">
+                <div class="img-container">
+                    <img src="<?php echo get_template_directory_uri(); ?>/assets/images/c1.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
     </div>
 
 
