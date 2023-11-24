ad059bab41238136bd73b3c9ca8503f014bd8184
yellhtut <yellhtut4@gmail.com>
2023-11-24 16:38:20 +0630

    push real time code

--- includes/footer.php
+++ includes/footer.php
@@ -11,9 +11,6 @@
             <div class="col-sm-12 col-md-4">
                 <p class="themes-para-fonts brandon-regular text-white mb-0">Atelier Sao Design Inc.</p>
                 <p class="themes-para-fonts brandon-regular text-white">A Creative Concept Driven International Award Winning Interior Architecture Studio</p>
-                <p class=" mt-5 text-white">
-                    <a class="text-white brandon-regular" href="https://futurenowmm.com/">Site by Future Now</a>
-                </p>
             </div>
             <div class="col-sm-12 col-md-4">
                 <div class="img-container d-flex justify-content-center ">
@@ -23,18 +20,22 @@
             <div class="col-sm-12 col-md-4 ">
                 <ul class="footer-listing d-flex justify-content-between align-items-center">
                     <li>
-                        <a class="text-white brandon-regular" href="<?php echo home_url(); ?>/contact">Contact Us</a>
+                        <a class="text-white brandon-regular" style="font-size: 20px!important;" href="<?php echo home_url(); ?>/contact">Contact Us</a>
                     </li>
                     <li>
-                        <a class="text-white brandon-regular" href="">Facebook</a>
+                        <a class="text-white brandon-regular" style="font-size: 20px!important;" href="">Facebook</a>
                     </li>
                     <li>
-                        <a class="text-white brandon-regular" href="">Instagram</a>
+                        <a class="text-white brandon-regular" style="font-size: 20px!important;" href="">Instagram</a>
                     </li>
                 </ul>
-                <p style="margin-top: 80px" class="themes-para-fonts brandon-regular text-center text-white">All images @ Atelier Sao</p>
-
             </div>
+        </div>
+        <div style="margin-top: 50px;" class="d-flex justify-content-between align-items-center">
+            <p class="text-white">
+                <a class="text-white brandon-regular" style="font-size: 18px;" href="https://futurenowmm.com/">Site by Future Now</a>
+            </p>
+            <p style="font-size: 18px" class="themes-para-fonts brandon-regular text-center text-white">All images @ Atelier Sao</p>
         </div>
     </div>
 </footer>
