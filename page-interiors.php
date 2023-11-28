972b147ee0f925a4a65e0459b0ae2fe65e39e8d9
yellhtut <yellhtut4@gmail.com>
2023-11-28 16:10:15 +0630

    push interior ui page

---
+++ page-interiors.php
@@ -0,0 +1,179 @@
+<?php include 'includes/head.php'?>
+<style>
+    .same-gray-card
+    {
+        background: #F0F0EC;
+        text-align: center;
+    }
+    p{
+        opacity: 0.6;
+    }
+    .gray-card-content
+    {
+        padding: 50px;
+    }
+</style>
+<body class="body-bg">
+<?php include 'includes/header.php'?>
+<div class="theme-container mt-4">
+    <div data-aos="fade-up" data-aos-duration="1000" class="img-container mb-3 pb-3">
+        <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/interior-banner.jpg" alt="">
+    </div>
+    <p data-aos="fade-up" data-aos-duration="1000" class="text-center themes-title-fonts mt-2 mb-4"><b>ATELIER DESIGN INC.</b></p>
+    <div class="row mb-5">
+        <div class="col-sm-12 col-md-4">
+            <p data-aos="fade-up" data-aos-duration="1000" class="themes-para-fonts my-5" >Atelier Sao Design Inc is a luxury interior architecture studio offering residential and commercial interior design and architecture services for private clients</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <p data-aos="fade-up" data-aos-duration="1000" class="themes-para-fonts my-5" >Founded in 2016 as an interior design studio, the Yangon-based an interdisciplinary brand has evolved into an interior architecture firm that is practicing the contemporary design conversation of Myanmar with diversified investments in Architecture, Construction, and Property Development.</p>
+        </div>
+        <div class="col-sm-12 col-md-4">
+            <p data-aos="fade-up" data-aos-duration="1000" class="themes-para-fonts my-5" >The studio presently consists of a team of creative individuals who share design expertise and inspiration through a wide range of skills such as Interior Design, Interior Architecture, Product Design, Visual Merchandising, Interior Styling, Creative Interior Branding, Environmental Graphic Application, and Architectural Integration.</p>
+        </div>
+    </div>
+    <p class="text-center themes-title-fonts mt-2 mb-4"><b>SERVICES & EXPERTISE</b></p>
+    <div class="row">
+        <div data-aos="fade-up" data-aos-duration="1000"  class="col-sm-12 col-md-6">
+            <div class="same-gray-card p-5">
+                <p>DESIGN MANAGEMENT</p>
+                <p>BESPOKE FURNITURE PRODUCTION</p>
+                <p>INTERIOR GRAPHIC DESIGN & INSTALLATION</p>
+                <p>LIGHTING DESIGN</p>
+            </div>
+        </div>
+        <div data-aos="fade-up" data-aos-duration="1000"  class="col-sm-12 col-md-6">
+            <div class="same-gray-card p-5">
+                <p>ARCHITECTURE</p>
+                <p>INTERIOR ARCHITECTURE</p>
+                <p>INTERIOR STYLING</p>
+                <p>ART CONSULTATION</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row mt-4 ">
+        <div class="col-sm-12 col-md-6 mx-0 px-0" data-aos="fade-up" data-aos-duration="1000" >
+            <div class="img-container">
+                <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/computer-on-chair.jpg" alt="">
+            </div>
+        </div>
+        <div class="col-sm-12 col-md-6 same-gray-card" data-aos="fade-up" data-aos-duration="1000" >
+            <div class="gray-card-content">
+                <p style="opacity: 1;" class="text-center themes-title-fonts mt-4 mb-4"><b>CULTURE</b></p>
+                <p class="themes-para-fonts">Atelier Sao is a culturally diverse team of capable, creative and committed people that combine to create an unbelievable collaborative group effort in creating unmatchable design. We strive to seek design solutions that in turn assist us in developing stunning projects that influence our community in an extremely advantageous way.
+                </p>
+                <p class="themes-para-fonts my-4">We create lifestyles and unique environments that are practical for our clients' needs and continuously strive to be informed and educated in design and product.</p>
+                <p class="themes-para-fonts">Atelier Sao is an Equal Opportunity Employer and has an ongoing commitment to fostering a diverse and inclusive workplace.</p>
+            </div>
+        </div>
+    </div>
+
+    <p style="opacity: 1;padding: 100px 0 30px 0;" class="text-center themes-title-fonts mt-5 mb-4"><b>WE THE TEAM</b></p>
+    <div data-aos="fade-up" data-aos-duration="1000" class="img-container">
+        <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/gp-photo.jpg" alt="">
+    </div>
+    <p class="themes-para-fonts w-75 m-auto my-3">The studio presently consists of a team of creative individuals who share design expertise and inspiration through a wide range of skills such as Interior Design, Interior Architecture, Product Design, Visual Merchandising, Interior Styling, Creative Interior Branding, Environmental Graphic Application, and Architectural Integration.</p>
+
+    <div class="row my-5">
+        <div class="col-sm-12 col-md-6">
+            <p style="opacity: 1;" class="text-center themes-title-fonts mt-5 mb-4"><b>CLIENTELE</b></p>
+            <div data-aos="fade-up" data-aos-duration="1000"  class="row client-images-place">
+<!--                <div class="col-sm-4 col-md-3">-->
+<!--                    <div class="img-container">-->
+<!--                        <img class="w-100 h-auto" src="--><?php //echo get_template_directory_uri(); ?><!--/assets/images/clients/1.jpg" alt="">-->
+<!--                    </div>-->
+<!--                </div>-->
+            </div>
+            <p data-aos="fade-up" data-aos-duration="1000" style="opacity: 1;" class="text-center"><b>See more</b></p>
+        </div>
+        <div class="col-sm-12 col-md-6">
+            <p style="opacity: 1;" class="text-center themes-title-fonts mt-5 mb-4"><b>FEATURED ON</b></p>
+            <div data-aos="fade-up" data-aos-duration="1000"  class="row mt-4 mb-5">
+                <div class="col-sm-12 col-md-4">
+                    <div class="img-container">
+                        <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/featured/f1.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="img-container">
+                        <img class="w-100 h-auto" src="<?php echo get_template_directory_uri(); ?>/assets/images/featured/f2.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4">
+                    <div class="img-container">
+                        <img class="w-50" style="height: 90px" src="<?php echo get_template_directory_uri(); ?>/assets/images/featured/f3.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">Design Excellence Award 2015:</p>
+                <p class="text-center themes-para-fonts">The award of Gold in Best Commercial Design</p>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">Success by Design 2016:</p>
+                <p class="text-center themes-para-fonts">Under 25 Designers</p>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">Moda Magazine September 2017:</p>
+                <p class="text-center themes-para-fonts">Moda Woman</p>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">Design Excellence Award 2017:</p>
+                <p class="text-center themes-para-fonts">The award of Silver in Best Residential Design</p>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">A' Design Award Nomination 2020:
+                <p class="text-center themes-para-fonts">4E Apartment Project</p>
+            </div>
+
+            <div data-aos="fade-up" data-aos-duration="1000"  class="each-para">
+                <p class="text-center themes-para-fonts mt-4 mb-0 pb-0">A' Design Award Nomination 2020:
+                <p class="text-center themes-para-fonts">Oriental Villa Project</p>
+            </div>
+        </div>
+    </div>
+
+</div>
+
+<?php include 'includes/desk-menu-overlay.php'?>
+
+<?php include 'includes/footer.php'?>
+<?php include 'includes/footer-script.php'?>
+
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/owl.carousel.min.js" ></script>
+<script src="<?php echo get_template_directory_uri(); ?>/assets/js/owl-setup.js" ></script>
+<script>
+
+    $(document).ready(function() {
+        AOS.init();
+    });
+
+
+    if (screen.width < 600) {
+        $('.for-mobile').removeAttr("src");
+        console.log('remove');
+        $('.hambager-list').remove();
+        $('.remove-style').removeAttr("style");
+        $('.mobile-hidden').addClass('d-none');
+        $('.mobile-wrap-title').html('df');
+    }
+    const wpPrefix = "<?php echo get_template_directory_uri(); ?>";
+
+    for (let i=0; i<9; i++)
+    {
+        $('.client-images-place').append(`<div class="col-sm-4 col-md-3 mb-4">
+                    <div class="img-container">
+                        <img class="w-100 h-auto" src="${wpPrefix}/assets/images/clients/${i+1}.jpg" alt="">
+                    </div>
+                </div>`);
+    }
+
+</script>
+
+</body>
+</html>
